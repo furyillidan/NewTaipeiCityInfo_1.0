79304cc201c58a3de1e6f381a0241b63fa41210b
NeoChou <killerkd@gmail.com>
2019-10-09 14:18:14 +0800

    upload version

--- Supporting Files/AppDelegate.swift
+++ Supporting Files/AppDelegate.swift
@@ -19,7 +19,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         GADMobileAds.sharedInstance().start(completionHandler: nil)
-        
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = .light
+        } else {
+            // Fallback on earlier versions
+        }
         return true
     }
 
